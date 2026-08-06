7cf97be65201e09b8f5c86f4ec282b28923fede3
IsraelConti <83127483+IsraelConti@users.noreply.github.com>
2026-08-06 21:39:23 +0200

    Fix application ID assignment in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
         buildConfig = true
     }
     defaultConfig {
-        ID de la aplicación = "com.icapps.maintenpro"
+        applicationId = "com.icapps.maintenpro"
         minSdk = 29
         targetSdk = 36
         versionCode = 4
